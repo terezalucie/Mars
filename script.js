e29d2b821d9dc2a07203bca8c987ff48d150c148
Tereza Kolaříková <tereza.lucie@protonmail.com>
2024-03-03 09:49:42 +0100

    02

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 // MARS - cvičení mimozemský život
-const dnes = dayjs()
+
+const dnes = dayjs().format("DD.MM.YYYY")
 const budoucnost = dayjs("2034-07-21")
 const selektor = document.querySelector(".odpoved")
-const jakyJeDen = prompt("Zadej dnešní datum ve formátu (YYYY-MM-DD):", dayjs())
+const jakyJeDen = prompt("Zadej dnešní datum ve formátu (YYYY-MM-DD):", dayjs().format("DD.MM.YYYY"))
 
-if(budoucnost.isAfter(jakyJeDen)){
+if(budoucnost.isAfter(jakyJeDen)) {
     selektor.textContent = "NE"
     selektor.style.color = "#9b0000"
-} else{
+} else {
     selektor.textContent = "ANO"
     selektor.style.color = "green"
 }
